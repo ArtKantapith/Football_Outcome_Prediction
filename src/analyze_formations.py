93c2c08c1ec4cc50659cf172598bbd6b7e36418a
Abhishek Jindal <ajindal121@gmail.com>
2017-04-30 15:23:23 -0400

    updating analyzing formations code

--- src/analyze_formations.py
+++ src/analyze_formations.py
@@ -1,11 +1,24 @@
 from get_formation_and_plot import get_formation
+from collections import defaultdict
 
 match_min_id  = 1
 match_max_id = 25979
 
+
+
 def analyze():
+    away_formation_dict = defaultdict(int)
+    home_formation_dict = defaultdict(int)
     for match_id in xrange(match_min_id, match_max_id):
         formations = get_formation(match_id)
-    print formations
+        if(formations == None):
+            continue
+        home_formation = formations[0]
+        away_formation = formations[1]
+        home_formation_dict[home_formation] += 1
+        away_formation_dict[away_formation] += 1
 
-analyze()
+    return home_formation_dict, away_formation_dict
+
+#home_dict, away_dict = analyze()
+#print home_dict
